8be63d89f3e09b4cd3795941f74eaa474131b470
Ig@reK <kigorek@mail.ru>
2015-05-26 19:50:57 +0300

    add controller pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 PageApp::Application.routes.draw do
+  get "sample_pages/home"
+  get "sample_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
